fd551051e697727c53d54edcf6dd5654fb08a937
vahitova <47237645+vahitova@users.noreply.github.com>
2021-03-24 02:29:13 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,28 +1,23 @@
 from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import CallbackContext, CommandHandler
 from telegram import ReplyKeyboardMarkup
-
+k = 0
 
 def echo(update, context):
-    update.message.reply_text(update.message.text)
+    global k
+    mess = ['вы действительно мой самый лучший класс', 'Хотя нет, лучшая группа', 'Настоящие айтишники', 'Все у вас будет хорошо', 'Все получится', 'Вы все поступите', 
+            'И как бы вы меня не троллили', 'Точнее мумитроллили', 'Я вас очень люблю!', 'А все потому что...']
+    k += 1
+    update.message.reply_text(mess[k%10])
 
 def help(update, context):
     update.message.reply_text(
-        "Я пока не умею помогать... Я только ваше эхо.")
-
-def start(update, context):
-    reply_keyboard = [['/start', '/help']]
-    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
-    update.message.reply_text(
-        "Привет! Я эхо-бот. Напишите мне что-нибудь, и я пришлю это назад!", reply_markup=markup)
-
-
+        "Екатерина Юрьевна спешит на помощь!")
 
 def main():
     updater = Updater('1796737799:AAHlxHF7n8tYerY1Xz0j1tVUbid1RoCGjRQ', use_context=True)
     dp = updater.dispatcher
     text_handler = MessageHandler(Filters.text, echo)
-    dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(text_handler)
     updater.start_polling()
